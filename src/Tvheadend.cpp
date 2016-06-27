e1a13ba26a82690868e9038db1dc1a6f0e239502
Kai Sommerfeld <kai.sommerfeld@gmx.com>
2016-06-27 19:57:35 +0200

    Fix setting channel type for 'early' htsp v25 servers.

--- src/Tvheadend.cpp
+++ src/Tvheadend.cpp
@@ -1818,12 +1818,20 @@ void CTvheadend::ParseChannelAddOrUpdate ( htsmsg_t *msg, bool bAdd )
         continue;
 
       /* Channel type */
+      bool bGotContent = false;
       if (m_conn.GetProtocol() >= 25)
       {
         if (!htsmsg_get_u32(&f->hmf_msg, "content", &u32))
+        {
           channel.SetType(u32);
+          bGotContent = true;
+        }
       }
-      else
+
+      // The 'content' htsp method field was added to tvheadend htsp api without htsp version bump.
+      // Unfortunately, there are many semi-official tvheadend builds with htsp version 25 in the wild which
+      // do not support the 'content' htsp attribute. Just checking htsp api version is not sufficient. :-/
+      if (!bGotContent)
       {
         if ((str = htsmsg_get_str(&f->hmf_msg, "type")) != NULL)
         {
