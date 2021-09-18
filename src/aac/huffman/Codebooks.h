47f68fc6977b0feaac284b5b2cb97b58a292c1c2
Alwin Esch <alwin.esch@web.de>
2021-09-18 18:58:19 +0200

    update copyright year to 2021

--- src/aac/huffman/Codebooks.h
+++ src/aac/huffman/Codebooks.h
@@ -1,10 +1,9 @@
 /*
-*  Copyright (C) 2005-2021 Team Kodi
-*  https://kodi.tv
-*
-*  SPDX-License-Identifier: GPL-2.0-or-later
-*  See LICENSE.md for more information.
-*/
+ *  Copyright (C) 2005-2021 Team Kodi (https://kodi.tv)
+ *
+ *  SPDX-License-Identifier: GPL-2.0-or-later
+ *  See LICENSE.md for more information.
+ */
 
 #pragma once
 
